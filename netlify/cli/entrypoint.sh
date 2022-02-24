6434f539e271a062845546f618b131fe96120bc5
Nelson Estevão <nelsonmestevao@gmail.com>
2022-02-24 14:43:52 +0000

    Improve Netlify cli action

--- netlify/cli/entrypoint.sh
+++ netlify/cli/entrypoint.sh
@@ -1,21 +1,19 @@
 #!/bin/sh -l
 
-# read -d '' COMMAND <<- EOF
-#   if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
-#     echo "$BUILD_DIR didn't change"
-#   else
-#     ${BUILD_COMMAND:-echo} && netlify $*
-#   fi
-# EOF
+read -d '' COMMAND <<- EOF
+  if [ -f "$HOME/ignore" ] && grep "^ignore:$BUILD_DIR" "$HOME/ignore"; then
+    echo "$BUILD_DIR didn't change"
+  else
+    ${BUILD_COMMAND:-echo} && netlify $*
+  fi
+EOF
 
-npm ci
-
-OUTPUT=$(sh -c "npm run preview")
+OUTPUT=$(sh -c "$COMMAND")
 
 NETLIFY_OUTPUT=$(echo "$OUTPUT")
-NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*') #Unique key: --
-NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*') #Unique key: app.netlify.com
-NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.com") #Unique key: don't containr -- and app.netlify.com
+NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*')
+NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*')
+NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.app")
 
 echo "::set-output name=NETLIFY_OUTPUT::$NETLIFY_OUTPUT"
 echo "::set-output name=NETLIFY_URL::$NETLIFY_URL"
