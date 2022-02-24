de337e09666b60d850ccd1671b342f5314b3f313
Nelson Estevão <nelsonmestevao@gmail.com>
2022-02-24 15:08:08 +0000

    Another attempt

--- netlify/cli/entrypoint.sh
+++ netlify/cli/entrypoint.sh
@@ -1,6 +1,6 @@
-#!/bin/sh -l
+#!/bin/sh
 
-OUTPUT=$(netlify $*)
+OUTPUT=$(netlify $@)
 
 NETLIFY_OUTPUT=$(echo "$OUTPUT")
 NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*')
