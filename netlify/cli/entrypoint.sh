cc6b79e133f51b0e674dd1e5d8d0bed42305b885
Nelson Estevão <nelsonmestevao@gmail.com>
2022-02-24 15:14:40 +0000

    Another attempt

--- netlify/cli/entrypoint.sh
+++ netlify/cli/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-OUTPUT=$(netlify $@)
+OUTPUT=$(sh -c "netlify $*")
 
-NETLIFY_OUTPUT=$(echo "$OUTPUT")
+NETLIFY_OUTPUT="$OUTPUT"
 NETLIFY_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*(--)[a-zA-Z0-9./?=_-]*')
 NETLIFY_LOGS_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://app.netlify.com/[a-zA-Z0-9./?=_-]*')
 NETLIFY_LIVE_URL=$(echo "$OUTPUT" | grep -Eo '(http|https)://[a-zA-Z0-9./?=_-]*' | grep -Eov "netlify.app")
